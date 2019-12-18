b912718b893d0725191e2d8b33d753ecc74be39b
ColeG135 <50749032+ColeG135@users.noreply.github.com>
2019-12-18 13:34:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
+function firstLast6(param1, param2) {
+    return param1
+}
+
+function has_23(arr, n) {
+    return arr;
+}
+
+function fix_23(arr, n) {
+    return arr;
+}
+
+function countYZ(str, n) {
+    return str;
+}
+
+function endOther(str2, n) {
+    return str2;
+}
+
+function starOut(str, n) {
+    return str;
+}
+
+function getSandwich(arr, n) {
+    return arr;
+}
+
+function canBalance(param1, param2) {
+    return param1;
+}
+
+function countClumps (arr, n) {
+    return arr;
+}
+
+function evenlySpaced (param1, param2) {
+    return param1;
+}
+
+function tester() {
+    document.getElementById("output").innerHTML += firstLast6(true, false);
+    document.getElementById("output").innerHTML += has_23(true, false);
+    document.getElementById("output").innerHTML += fix_23('[1,2,3]', '[2,3,5]', '[1,2,1]');
+    document.getElementById("output").innerHTML += countYZ('fez day', 'day fez', 'day fyyyz');
+    document.getElementById("output").innerHTML += endOther(true, false);
+    document.getElementById("output").innerHTML += starOut('ad', 'ad', 'silly');
+    document.getElementById("output").innerHTML += getSandwich('jam', 'jam' , "");
+    document.getElementById("output").innerHTML += canBalance(true, false);
+    document.getElementById("output").innerHTML += countClumps('2', '2', '1');
+    document.getElementById("output").innerHTML += evenlySpaced(true, false);
+
+
+document.getElementById("output").innerHTML += firstLast6(true, false);
+document.getElementById("output").innerHTMl += has_23(true, false);
+document.getElementById("output").innerHTMl += fix_23('[1,2,3]', '[2,3,5]', '[1,2,1]');
+document.getElementById("output").innerHTML += countYZ('fez day', 'day fez', 'day fyyyz');
+document.getElementById("output").innerHTML += endOther(true, false);
+document.getElementById("output").innerHTML += starOut ('ad', 'ad', 'silly');
+document.getElementById("output").innerHTML += getSandWich('jam', 'jam', "");
+document.getElementById("output").innerHTML += canBalance(true, false);
+document.getElementById("output").innerHTML += countClumps('2', '2', '1');
+document.getElementById("output").innerHTML += evenlySpaced(true, false);
+}
 
